7cc79cf740f3bcb35c794d84ef93d1785428dd0b
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2020-10-04 13:18:08 +0100

    - Create separate Rater classes

---
+++ ArdalisRating/AutoPolicyRater.cs
@@ -0,0 +1,36 @@
+﻿namespace ArdalisRating
+{
+    public class AutoPolicyRater
+    {
+        private readonly RatingEngine _rateEngine;
+        private readonly ConsoleLogger _logger;
+
+        public AutoPolicyRater(RatingEngine ratingEngine, ConsoleLogger logger)
+        {
+            _rateEngine = ratingEngine;
+            _logger = logger;
+        }
+
+        public void Rate(Policy policy)
+        {
+            _logger.Log("Rating AUTO policy...");
+            _logger.Log("Validating policy.");
+
+            if (string.IsNullOrEmpty(policy.Make))
+            {
+                _logger.Log("Auto policy must specify Make");
+                return;
+            }
+
+            if (policy.Make == "BMW")
+            {
+                if (policy.Deductible < 500)
+                {
+                    _rateEngine.Rating = 1000m;
+                }
+
+                _rateEngine.Rating = 900m;
+            }
+        }
+    }
+}
